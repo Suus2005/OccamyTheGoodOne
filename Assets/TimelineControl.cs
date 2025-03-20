a9630ad09f8e8a80f609eb15fc7613a190b58d55
Suus2005 <Susanwinkels34@gmail.com>
2025-03-20 11:54:55 +0100

    Buttons berken voor eerste animatie

---
+++ Assets/TimelineControl.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Playables;
+
+public class TimelineControl : MonoBehaviour
+{
+
+    public PlayableDirector playableDirector;  // Reference to Playable Director
+    public Button playButton;                  // Reference to the play button
+    public Button stopButton;                  // Reference to the stop button
+
+    void Start()
+    {
+        // Add listeners to buttons to play or stop the timeline when clicked
+        playButton.onClick.AddListener(PlayTimeline);
+        stopButton.onClick.AddListener(StopTimeline);
+    }
+
+    void PlayTimeline()
+    {
+        if (playableDirector != null)
+        {
+            // Play the timeline
+            playableDirector.Play();
+        }
+    }
+
+    void StopTimeline()
+    {
+        if (playableDirector != null)
+        {
+            // Stop the timeline (it will reset to the first frame)
+            playableDirector.Stop();
+        }
+    }
+}
