f3c6898033491e3eea519b83854c5d720268d6b4
Suus2005 <Susanwinkels34@gmail.com>
2025-03-25 16:50:22 +0100

    urp
    skybox, terain, animation kinda work and project to urp

--- Assets/TimelineControl.cs
+++ Assets/TimelineControl.cs
@@ -8,38 +8,43 @@ public class TimelineControl : MonoBehaviour
 {
 
     public PlayableDirector playableDirector;  // Reference to Playable Director
-    public Button playButton;                  // Reference to the play button
-    public Button stopButton;                  // Reference to the stop button
+    public Button Kronkelen;                  // Reference to the play button
+    public Button stop;                  // Reference to the stop button
     public Button Idle;
+  
 
     void Start()
     {
         // Add listeners to buttons to play or stop the timeline when clicked
-        playButton.onClick.AddListener(PlayTimeline);
-        stopButton.onClick.AddListener(StopTimeline);
-       
+        Kronkelen.onClick.AddListener(PlayTimeline);
+        stop.onClick.AddListener(StopTimeline);
+
     }
 
-   
+
 
     void PlayTimeline()
     {
         if (playableDirector != null)
         {
+           
             // Play the timeline
             playableDirector.Play();
         }
     }
 
-    
+
 
     void StopTimeline()
     {
         if (playableDirector != null)
         {
-            // Stop the timeline (it will reset to the first frame)
+           
             playableDirector.Stop();
-            
+            // Destroy(OccamyKronkel);
+
+
         }
     }
+
 }
